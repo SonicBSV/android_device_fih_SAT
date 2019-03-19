c366e50a9fe4c5f68ef354009532bbd029314f59
SonicBSV <sonicbsv@gmail.com>
2019-03-19 08:08:59 +0300

    add missing android.hardware.biometrics.fingerprint@2.1

--- device.mk
+++ device.mk
@@ -308,8 +308,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     drm.service.enabled=true
     
 # Fingerprint feature
-#PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service
+PRODUCT_PACKAGES += \
+    android.hardware.biometrics.fingerprint@2.1
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml \
